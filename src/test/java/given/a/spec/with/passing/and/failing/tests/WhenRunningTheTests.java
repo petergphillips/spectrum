bb85dd87968f14d896688f33178bfd63e3aa02eb
Peter Phillips <peter@greenthistle.co.uk>
2015-11-17 14:05:15 +0000

    Example of running spectrum with Mockito integration

--- src/test/java/given/a/spec/with/passing/and/failing/tests/WhenRunningTheTests.java
+++ src/test/java/given/a/spec/with/passing/and/failing/tests/WhenRunningTheTests.java
@@ -1,10 +1,15 @@
 package given.a.spec.with.passing.and.failing.tests;
 
+import static com.greghaskins.spectrum.Spectrum.afterEach;
+import static com.greghaskins.spectrum.Spectrum.beforeEach;
+import static com.greghaskins.spectrum.Spectrum.describe;
+import static com.greghaskins.spectrum.Spectrum.it;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.Description;
+import org.junit.runner.RunWith;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
 import org.mockito.InOrder;
@@ -13,36 +18,38 @@ import org.mockito.Mockito;
 import com.greghaskins.spectrum.Spectrum;
 
 public class WhenRunningTheTests {
-
     private static final String CONTEXT_NAME = "a spec with three passing and two failing tests";
 
-    private RunNotifier runNotifier;
+    private final RunNotifier runNotifier = mock(RunNotifier.class);
 
-    @Before
-    public void before() throws Exception {
-        final Runner runner = new Spectrum(Fixture.getSpecWithPassingAndFailingTests());
-        runNotifier = mock(RunNotifier.class);
+    {
+        describe("Start and finished notifications", () -> {
+            beforeEach(() -> {
+                final Runner runner = new Spectrum(Fixture.getSpecWithPassingAndFailingTests());
+                runner.run(runNotifier);
+            });
 
-        runner.run(runNotifier);
+            afterEach(() -> {
+                Mockito.validateMockitoUsage();
+                Mockito.reset(runNotifier);
+            });
+
+            it("Should fire the start failure and finished notifications for failing tests", () -> {
+                final Description descriptionOfFailingTest = Description.createTestDescription(CONTEXT_NAME, "fails test 1");
+
+                final InOrder inOrder = Mockito.inOrder(runNotifier);
+                inOrder.verify(runNotifier).fireTestStarted(descriptionOfFailingTest);
+                inOrder.verify(runNotifier).fireTestFailure(Mockito.any());
+                inOrder.verify(runNotifier).fireTestFinished(descriptionOfFailingTest);
+            });
+
+            it("Should fire the start and finished notifications for passing tests", () -> {
+                final Description descriptionOfPassingTest = Description.createTestDescription(CONTEXT_NAME, "passes test 3");
+
+                final InOrder inOrder = Mockito.inOrder(runNotifier);
+                inOrder.verify(runNotifier).fireTestStarted(descriptionOfPassingTest);
+                inOrder.verify(runNotifier).fireTestFinished(descriptionOfPassingTest);
+            });
+        });
     }
-
-    @Test
-    public void theStartFailureAndFinishedNotificationsAreFiredForFailingTests() throws Exception {
-        final Description descriptionOfFailingTest = Description.createTestDescription(CONTEXT_NAME, "fails test 1");
-
-        final InOrder inOrder = Mockito.inOrder(runNotifier);
-        inOrder.verify(runNotifier).fireTestStarted(descriptionOfFailingTest);
-        inOrder.verify(runNotifier).fireTestFailure(Mockito.any());
-        inOrder.verify(runNotifier).fireTestFinished(descriptionOfFailingTest);
-    }
-
-    @Test
-    public void theStartAndFinishedNotificationsAreFiredForPassingTests() throws Exception {
-        final Description descriptionOfPassingTest = Description.createTestDescription(CONTEXT_NAME, "passes test 3");
-
-        final InOrder inOrder = Mockito.inOrder(runNotifier);
-        inOrder.verify(runNotifier).fireTestStarted(descriptionOfPassingTest);
-        inOrder.verify(runNotifier).fireTestFinished(descriptionOfPassingTest);
-    }
-
 }
